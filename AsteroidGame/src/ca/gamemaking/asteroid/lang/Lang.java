59216e2d0dc086cf45e4ce078b33308fcb180bad
maxlajoie99 <max.lajoie99@hotmail.com>
2018-06-07 16:24:12 -0400

    Ajout de librairies Jackson

--- AsteroidGame/src/ca/gamemaking/asteroid/lang/Lang.java
+++ AsteroidGame/src/ca/gamemaking/asteroid/lang/Lang.java
@@ -3,14 +3,19 @@
  */
 package ca.gamemaking.asteroid.lang;
 
+import ca.gamemaking.asteroid.lang.json.LangReaderJSON;
+import java.util.Map;
+
 /**
  *
  * @author mlajoie
  */
 public class Lang {
     
+    Map<String, String> fields;
+    
     public Lang(String name){
-        
+        fields = LangReaderJSON.ReadFile(name);
     }
     
     public String getText(String text){
--- AsteroidGame/src/ca/gamemaking/asteroid/lang/json/LangReaderJSON.java
+++ AsteroidGame/src/ca/gamemaking/asteroid/lang/json/LangReaderJSON.java
@@ -3,10 +3,21 @@
  */
 package ca.gamemaking.asteroid.lang.json;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  *
  * @author mlajoie
  */
 public class LangReaderJSON {
+    
+    public static Map<String,String> ReadFile(String name){
+        Map<String,String> file = new HashMap<String, String>();
+       
+        
+        
+        return file;
+    }
     
 }
